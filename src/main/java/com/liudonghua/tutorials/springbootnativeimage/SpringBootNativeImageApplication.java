ebccd5894bccc8bb9f6197c426355559642863b2
liudonghua123 <liudonghua123@gmail.com>
2021-11-30 11:24:13 +0800

    initial spring boot project files

---
+++ src/main/java/com/liudonghua/tutorials/springbootnativeimage/SpringBootNativeImageApplication.java
@@ -0,0 +1,13 @@
+package com.liudonghua.tutorials.springbootnativeimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNativeImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNativeImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liudonghua/tutorials/springbootnativeimage/SpringBootNativeImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liudonghua.tutorials.springbootnativeimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNativeImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
