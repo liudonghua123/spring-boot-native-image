d89d01252371e701045097726ccbd8965c20a55b
liudonghua123 <liudonghua123@gmail.com>
2021-11-30 14:28:47 +0800

    fix native build

--- src/main/java/com/liudonghua/tutorials/springbootnativeimage/SpringBootNativeImageApplication.java
+++ src/main/java/com/liudonghua/tutorials/springbootnativeimage/SpringBootNativeImageApplication.java
@@ -2,12 +2,20 @@ package com.liudonghua.tutorials.springbootnativeimage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootNativeImageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNativeImageApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
 }
